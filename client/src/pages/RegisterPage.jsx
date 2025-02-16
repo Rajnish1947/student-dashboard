b25fe163978b85b86fe573a3947483847f83a194
Rajnish1947 <kumarrajnish28443@gmail.com>
2025-02-16 15:35:31 +0530

    Update RegisterPage.jsx

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -24,7 +24,7 @@ export default function RegisterPage() {
 
     try {
       const response = await axios.post(
-        'https://student-dashboard-8ca8.onrender.com',
+        'https://student-dashboard-1-oz0y.onrender.com',
         { name, email, password },
         { withCredentials: true }
       );
