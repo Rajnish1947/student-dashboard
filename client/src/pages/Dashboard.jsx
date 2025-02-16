50ff0619b8b850a306f08d4ebc03dcb3e2a765f8
Rajnish1947 <kumarrajnish28443@gmail.com>
2025-02-16 16:13:08 +0530

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
   useEffect(() => {
     const fetchProfileData = async () => {
       try {
-        const response = await axios.get("https://student-dashboard-8ca8.onrender.com/auth/profile", { withCredentials: true });
+        const response = await axios.get("https://student-dashboard-1-oz0y.onrender.com/profile", { withCredentials: true });
         setUser(response.data);
       } catch (err) {
         setError("Failed to fetch profile data");
