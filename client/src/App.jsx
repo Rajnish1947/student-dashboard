d0ea62e3743c9d5cfed2740423cdcf8bad5bf4f2
Rajnish1947 <kumarrajnish28443@gmail.com>
2025-02-16 15:36:16 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 
 import CalendarView from "./pages/Dashboard";
 
-axios.defaults.baseURL = "https://student-dashboard-8ca8.onrender.com";
+axios.defaults.baseURL = "https://student-dashboard-1-oz0y.onrender.com";
 axios.defaults.withCredentials = true;
 
 function App() {
